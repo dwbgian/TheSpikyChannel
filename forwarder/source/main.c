2d23716fdf781167f1dbb890c3ee1d8db6317950
dwbgian <gianschreiner@gmail.com>
2026-09-18 21:13:27 +0200

    Fix forwarder aligned allocation include

--- forwarder/source/main.c
+++ forwarder/source/main.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 #include <string.h>
 #include <wiiuse/wpad.h>
 
@@ -213,4 +214,3 @@ int main(int argc, char **argv)
     entry();
     return 0;
 }
-
